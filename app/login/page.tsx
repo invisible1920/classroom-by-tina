7acfec44f81449b53649d7936fa261de3fb3cb00
John McCauley <johnmccauley1920@gmail.com>
2026-06-26 20:50:49 -0400

    Add superbase

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -48,7 +48,7 @@ async function signInWithGoogle() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: "http://localhost:3000/auth/callback",
+      redirectTo: `${origin}/auth/callback`,
     },
   });
 
